f636a36a8da6440eae3efe90816b2c37771e5670
shubhampaithankar <shubhampaithankar65@gmail.com>
2024-07-31 16:53:39 +0530

    added db connection

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 import express from 'express'
-const app = express()
-
 import cors from 'cors'
-
+import mongoose from 'mongoose'
 import { config } from 'dotenv'
+
 config()
+const app = express()
 
 app.use(express.json())
 app.use(cors())
+
+mongoose.connect(process.env.MONGO_URL)
+    .then(() => console.log(`connected to mongo database: ${connection.connection.db.databaseName}`))
+    .catch(err => console.log(`mongoose connection error ${err}`))
+
 
 const PORT = 3001
 app.listen(PORT, () => console.log(`started on http://localhost:${PORT}`))
