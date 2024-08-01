8ac4c5cbc9fd39b78edc12b387e1529796fa948c
shubhampaithankar <shubhampaithankar65@gmail.com>
2024-08-01 12:11:56 +0530

    added pokemon route

--- src/routes/pokemon.js
+++ src/routes/pokemon.js
@@ -1,4 +1,8 @@
 import { Router } from 'express'
+import { GetGenerationPokemon, GetRandomPokemon } from '../controllers/pokemon.controller.js'
 const router = Router()
+
+router.post('/get-random', GetRandomPokemon)
+router.post('/get-by-generation', GetGenerationPokemon)
 
 export default router
