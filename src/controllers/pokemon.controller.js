4b0b13399901e8f49889ad9218ee862c1aaec5ab
shubhampaithankar <shubhampaithankar65@gmail.com>
2024-08-01 12:11:25 +0530

    added pokemon controller

--- src/controllers/pokemon.controller.js
+++ src/controllers/pokemon.controller.js
@@ -1,23 +1,45 @@
-import User from '../models/user.model.js'
+import lodash from 'lodash'
+
+const POKE_API = 'https://pokeapi.co/api/v2'
+const { slice, shuffle } = lodash
 
 export const GetRandomPokemon = async (req, res) => {
     try {
-        
-        // const { user_id } = req.query
-        
-        // const user = await User.findOne({
-        //     _id: user_id
-        // })
+        const response = await fetch(`${POKE_API}/pokemon?limit=1500`, {
+            method: 'GET'
+        })
+        const { results } = await response.json()
+        // const modifiedArray = slice(shuffle(results), 0, 9) 
 
-        // if (!user) return res.status(406).send({
-        //     ack: 0,
-        //     msg: 'User not found'
-        // })
+        res.status(200).send({
+            ack: 1,
+            results
+        })
 
-        // res.status(200).send({
-        //     ack: 1,
-        //     pokemon: user.pokemon
-        // })
+    } catch (err) {
+        res.status(500).send({
+            ack: 3,
+            error: err.message
+        })
+    }
+}
+
+export const GetGenerationPokemon = async (req, res) => {
+    try {
+        const { generation } = req.body
+
+        // https://pokeapi.co/api/v2/generation/{id or name}/
+        const response = await fetch(`${POKE_API}/generation/${generation}`, {
+            method: 'GET'
+        })
+
+        const { pokemon_species } = await response.json()
+        // const modifiedArray = slice(shuffle(pokemon_species), 0, 9) 
+
+        res.status(200).send({
+            ack: 1,
+            results: pokemon_species
+        })
 
     } catch (err) {
         res.status(500).send({
