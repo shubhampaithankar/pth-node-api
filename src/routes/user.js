7c97dacb206b93ddd90efe1ab9e1eb2d0625eb52
shubhampaithankar <shubhampaithankar65@gmail.com>
2024-08-01 12:12:09 +0530

    added user based routes

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,5 +1,14 @@
 import { Router } from 'express'
+import { AddPokemonToUser, DeletePokemonFromUser, GetAllPokemon, GetUser } from '../controllers/user.controller.js'
+
 const router = Router()
 
+router.post('/get', GetUser)
+
+router.post('/pokemon/get-all', GetAllPokemon)
+
+router.post('/pokemon/add', AddPokemonToUser)
+
+router.post('/pokemon/delete', DeletePokemonFromUser)
 
 export default router
