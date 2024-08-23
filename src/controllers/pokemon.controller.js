8db1b8af8df86b79b588895a59aef3ca9e1b9a5c
shubhampaithankar <shubhampaithankar65@gmail.com>
2024-08-23 12:58:34 +0530

    better fetching for better response + caching

--- src/controllers/pokemon.controller.js
+++ src/controllers/pokemon.controller.js
@@ -1,19 +1,43 @@
 import lodash from 'lodash'
+import Pokedex from 'pokedex-promise-v2';
 
-const POKE_API = 'https://pokeapi.co/api/v2'
+
+const options = {
+    cacheLimit: 100 * 1000, // 100s
+    timeout: 5 * 1000 // 5s
+}
+const P = new Pokedex(options)
+
 const { slice, shuffle } = lodash
+
+
 
 export const GetRandomPokemon = async (req, res) => {
     try {
-        const response = await fetch(`${POKE_API}/pokemon?limit=1500`, {
-            method: 'GET'
+        const { results }  = (await P.getResource(['api/v2/pokemon-species?limit=1500']))[0]
+        const pokemons = slice(shuffle(results), 0, 10).map(({ url }) => {
+            const parts = url.split('/')
+            const id = parts[parts.length - 2]
+            return id
         })
-        const { results } = await response.json()
-        // const modifiedArray = slice(shuffle(results), 0, 9) 
 
-        res.status(200).send({
-            ack: 1,
-            results
+        console.log(pokemons)
+
+        P.getPokemonByName(pokemons, (response, err) => {
+            if (err) throw err
+            const result = response.map(pokemon => ({
+                abilities: pokemon.abilities,
+                id: pokemon.id,
+                name: pokemon.name,
+                sprites: pokemon.sprites, 
+                stats: pokemon.stats,
+                types: pokemon.types
+            }))
+            res.status(200).send({
+                ack: 1,
+                result
+            })
+            
         })
 
     } catch (err) {
