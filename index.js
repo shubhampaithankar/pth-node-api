a9d5653a868bfd4a7a79cbcfe00d0832ca71154e
shubhampaithankar <shubhampaithankar65@gmail.com>
2024-08-28 12:51:17 +0530

    using httpserver

--- index.js
+++ index.js
@@ -1,21 +1,17 @@
-import express from 'express'
-import cors from 'cors'
+import http from 'http'
 import mongoose from 'mongoose'
 import { config } from 'dotenv'
-import router from './src/routes/router.js'
+
+import app from './src/app.js'
 
 config()
-const app = express()
 
-app.use(express.json())
-app.use(cors())
+const PORT = 3001
 
 mongoose.connect(process.env.MONGO_URL)
     .then((connection) => console.log(`connected to mongo database: ${connection.connection.db.databaseName}`))
     .catch(err => console.log(`mongoose connection error ${err}`))
 
 
-app.use('/api', router)
-
-const PORT = 3001
-app.listen(PORT, () => console.log(`started on http://localhost:${PORT}`))
+const server = http.createServer(app)
+server.listen(PORT, () => console.log(`started on http://localhost:${PORT}`))
