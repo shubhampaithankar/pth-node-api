971d9753bbfa4ddc697ef80441834bab4cd60cdc
shubhampaithankar <shubhampaithankar65@gmail.com>
2024-09-02 00:37:18 +0530

    added cookie parser package

--- src/app.js
+++ src/app.js
@@ -1,13 +1,14 @@
 import express from 'express'
 import cors from 'cors'
+import cookieParser from 'cookie-parser'
 
 import router from './routes/router.js'
-
 
 const app = express()
 
 app.use(express.json())
 app.use(cors())
+app.use(cookieParser())
 
 app.use('/api', router)
 
