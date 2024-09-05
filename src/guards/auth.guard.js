6053606cceb91505eb1962b4d3e2475528900a29
shubhampaithankar <shubhampaithankar65@gmail.com>
2024-09-05 15:34:08 +0530

    auth guard token changes

--- src/guards/auth.guard.js
+++ src/guards/auth.guard.js
@@ -2,7 +2,7 @@ import jwt from 'jsonwebtoken'
 
 export default (req, res, next) => {
     try{
-        const token = req.header('x-auth-token')
+        const token = req.header('Authorization').split(' ')[1]
         if(!token) return res.status(401).json({ ack: 0, error: 'No authentication token, access denied' })
         
         const verified = jwt.verify(token, process.env.JWT_ACCESS_SECRET)
