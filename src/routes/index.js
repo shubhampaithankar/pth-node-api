4f6d83c432c9b7b5e87f063e73672fc01decf98a
shubhampaithankar <shubhampaithankar65@gmail.com>
2024-09-05 15:35:21 +0530

    route naming changes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,9 @@
 import { Router } from 'express'
 
-import authRoute from './auth.js'
-import userRoute from './user.js'
-import pokemonRoute from './pokemon.js'
+import authRoute from './auth.route.js'
+import userRoute from './user.route.js'
+import pokemonRoute from './pokemon.route.js'
+// import messageRoute from './message.route.js'
 
 import authGuard from '../guards/auth.guard.js'
 
@@ -11,5 +12,6 @@ const router = Router()
 router.use('/auth', authRoute)
 router.use('/pokemon', authGuard, pokemonRoute)
 router.use('/user', authGuard, userRoute)
+// router.use('/message', authGuard, messageRoute)
 
 export default router
